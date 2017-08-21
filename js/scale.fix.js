0ad025b447bd241d6786c725cc2562b0464f1fc6
kai2017 <kai2017@qq.com>
2017-08-21 22:40:40 +0800

    Site updated: 2017-08-21 22:40:39

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -1,17 +1,3 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+// build time:Mon Aug 21 2017 22:36:08 GMT+0800 (中国标准时间)
+var metas=document.getElementsByTagName("meta");var i;if(navigator.userAgent.match(/iPhone/i)){for(i=0;i<metas.length;i++){if(metas[i].name=="viewport"){metas[i].content="width=device-width, minimum-scale=1.0, maximum-scale=1.0"}}document.addEventListener("gesturestart",gestureStart,false)}function gestureStart(){for(i=0;i<metas.length;i++){if(metas[i].name=="viewport"){metas[i].content="width=device-width, minimum-scale=0.25, maximum-scale=1.6"}}}
+//rebuild by neat 
