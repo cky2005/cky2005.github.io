de0fc29d6f61fc26b35a4e586f4a56fe79252851
kai2017 <kai2017@qq.com>
2017-08-24 16:56:06 +0800

    Site updated: 2017-08-24 16:56:05

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 22 2017 20:40:13 GMT+0800 (中国标准时间)
+// build time:Thu Aug 24 2017 16:55:56 GMT+0800 (中国标准时间)
 var metas=document.getElementsByTagName("meta");var i;if(navigator.userAgent.match(/iPhone/i)){for(i=0;i<metas.length;i++){if(metas[i].name=="viewport"){metas[i].content="width=device-width, minimum-scale=1.0, maximum-scale=1.0"}}document.addEventListener("gesturestart",gestureStart,false)}function gestureStart(){for(i=0;i<metas.length;i++){if(metas[i].name=="viewport"){metas[i].content="width=device-width, minimum-scale=0.25, maximum-scale=1.6"}}}
 //rebuild by neat 
